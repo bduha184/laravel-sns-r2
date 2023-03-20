6d9781ee20f1b6af8713e37b527b96b75effb55e
bduha184 <bduha_184@yahoo.co.jp>
2023-03-20 23:07:23 +0900

    記事更新画面作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -25,4 +25,9 @@ class ArticleController extends Controller
 
         return redirect()->route('articles.index');
     }
+
+    public function edit(Article $article){
+        return view('articles.edit',compact('article'));
+    }
+
 }
