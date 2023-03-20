7b47ff1037dbc2611e48df15b8101209cd2e4afc
bduha184 <bduha_184@yahoo.co.jp>
2023-03-20 14:28:24 +0900

    Route::resourceを設定し、articles.indexが被っているため、 resourceからexceptでindexを除外

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', [ArticleController::class,'index'])->name('articles.index');
+Route::resource('/articles', ArticleController::class)->except(['index']);
