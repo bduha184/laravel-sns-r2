0cdc78e4f73483396808c8b6a85317f6dc0967f6
bduha184 <bduha_184@yahoo.co.jp>
2023-03-20 23:16:25 +0900

    記事削除実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -35,4 +35,9 @@ class ArticleController extends Controller
         $article->fill($request->all())->save();
         return redirect()->route('articles.index');
     }
+
+    public function destroy(Article $article){
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
 }
